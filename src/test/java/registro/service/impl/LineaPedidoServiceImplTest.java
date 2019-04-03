2dec340b2e3e46347130f52254537e0eb833ff32
tilan04 <2202kx@gmail.com>
2019-04-03 19:13:58 +0200

    Revert "Testing"
    This reverts commit 93c85a6690e87b5e94716d1d070195e348d18cd8.

--- src/test/java/registro/service/impl/LineaPedidoServiceImplTest.java
+++ src/test/java/registro/service/impl/LineaPedidoServiceImplTest.java
@@ -1,33 +1,21 @@
 package registro.service.impl;
 
-import static org.junit.Assert.assertNull;
+import java.util.List;
 
 import org.junit.Before;
 import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.jdbc.datasource.embedded.EmbeddedDatabase;
-import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
-import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
-import org.springframework.test.context.junit4.SpringRunner;
+import static org.assertj.core.api.Assertions.*;
+import static org.junit.Assert.assertNull;
 
-import es.curso.registro.Application;
 import es.curso.registro.model.Estado;
 import es.curso.registro.model.LineaPedido;
 import es.curso.registro.model.Pedido;
 import es.curso.registro.model.Product;
 import es.curso.registro.service.LineaPedidoService;
 
-
-@RunWith(SpringRunner.class)
-@SpringBootTest(classes={Application.class})
 public class LineaPedidoServiceImplTest {
 
-	@Autowired
 	private LineaPedidoService lineaPedidoService;
-    private EmbeddedDatabase db;
-
 	private LineaPedido lineaPedidoSim;
 	private Product productoSim;
 	private Pedido pedidoSim;
@@ -37,19 +25,11 @@ public class LineaPedidoServiceImplTest {
 	
 	@Before
 	public void setup() {
-//		estadoSim = new Estado(999, "Estado Sim");
-//		productoSim = new Product(999, "descripcion Sim", "marca Sim", "nombre Sim", 999);
-//		pedidoSim = new Pedido(999, "comentario Sim", "direccion Sim", estadoSim, precioFinal);
-//		lineaPedidoSim = new LineaPedido(999, cantidad, pedidoSim, productoSim);
-//		lineaPedidoService.addLineaPedido(lineaPedidoSim);
-		
-		
-    	db = new EmbeddedDatabaseBuilder()
-        		.setType(EmbeddedDatabaseType.H2)
-        		.addScript("create-db.sql")
-        		.addScript("insert-data.sql")
-        		.build();
-
+		estadoSim = new Estado(999, "Estado Sim");
+		productoSim = new Product(999, "descripcion Sim", "marca Sim", "nombre Sim", 999);
+		pedidoSim = new Pedido(999, "comentario Sim", "direccion Sim", estadoSim, precioFinal);
+		lineaPedidoSim = new LineaPedido(999, cantidad, pedidoSim, productoSim);
+		lineaPedidoService.addLineaPedido(lineaPedidoSim);
 	}
 	
 	@Test
