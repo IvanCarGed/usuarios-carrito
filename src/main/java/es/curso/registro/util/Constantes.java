6a540e2d81bffb97e5256f7e811144d9d2cd1545
tilan04 <2202kx@gmail.com>
2019-04-04 20:44:34 +0200

    Cambios diseño, añadir roles nuevos.

--- src/main/java/es/curso/registro/util/Constantes.java
+++ src/main/java/es/curso/registro/util/Constantes.java
@@ -6,17 +6,15 @@ public class Constantes {
 	
 	public static final String USER = "USER";
 	
+	public static final String DINAMIZADOR = "DINAMIZADOR";
+	
+	public static final String ENCARGADO = "ENCARGADO";
+	
 	public static final String ROLE_ADMIN = "ROLE_ADMIN";
 	
 	public static final String ROLE_USER = "ROLE_USER";
 	
-	public static final String CREADO = "Creado";
+	public static final String ROLE_DINAMIZADOR = "ROLE_DINAMIZADOR";
 	
-	public static final String PAGADO = "Pagado";
-	
-	public static final String CANCELADO = "Cancelado";
-	
-	public static final String ENVIADO = "Enviado";
-	
-	public static final String FINALIZADO = "Finalizado";
+	public static final String ROLE_ENCARGADO = "ROLE_ENCARGADO";
 }
